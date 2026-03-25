5a24a0ac39106acc6a7da8c51261c7c50493f881
gmandres10 <gman3david@gmail.com>
2026-03-25 19:28:40 +0000

    Mar 25, 2026, 3:28 PM

--- src/main.py
+++ src/main.py
@@ -66,10 +66,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    # Update game state
+    player.update()
+    
     # Drawing
     draw_text("Welcome to Single Shot Arena!", BLUE, (10, 10))
     pygame.draw.circle(screen, (RED), (300,300), 20)
     pygame.draw.rect(screen, (GREEN), (400, 300, 100, 200))
+    
+    # Draw player
+    screen.blit(player.image, player.rect)
+    
     # Update display
     pygame.display.flip()
 
