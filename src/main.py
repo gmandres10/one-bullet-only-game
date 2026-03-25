1e682f1728dd460b1af91e31246410509ba2f6ee
gmandres10 <gman3david@gmail.com>
2026-03-25 19:11:14 +0000

    Mar 25, 2026, 3:11 PM

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 import pygame, os 
 
+GAME_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_asset_path(filename: str) -> str:
+    '''Returns the path to an asset file, given its filename.'''
+    return os.path.join(GAME_PATH, "assets", filename)
+
+
 # Initialize Pygame
 pygame.init()
 
