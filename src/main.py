404c11a997d502bd84da1852a09f060d41ce4b03
gmandres10 <gman3david@gmail.com>
2026-03-25 03:34:14 +0000

    Mar 24, 2026, 11:34 PM

--- src/main.py
+++ src/main.py
@@ -7,3 +7,11 @@ GAME_TITLE = "Single Shot Arena"
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 MAX_FPS = 60
+
+
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption(GAME_TITLE)
