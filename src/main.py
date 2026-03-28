75ad86e586cb3e4b4417f951049bb01469ce5576
gmandres10 <gman3david@gmail.com>
2026-03-28 03:09:23 +0000

    Mar 27, 2026, 11:09 PM

--- src/main.py
+++ src/main.py
@@ -109,8 +109,7 @@ while running:
     all_sprites.draw(screen)
     
     # Draw floor
-    pygame.draw.line(screen, BLACK, (0, FLOOR_Y, SCREEN_WIDTH, FLOOR_Y), 3)
-    
+    pygame.draw.line(screen, BLACK, (0, FLOOR_Y), (SCREEN_WIDTH, FLOOR_Y), 2)   
     draw_text("Welcome to Single Shot Arena!", BLUE, (10, 10))
     pygame.draw.circle(screen, (RED), (300,300), 20)
     pygame.draw.rect(screen, (GREEN), (400, 300, 100, 200))
