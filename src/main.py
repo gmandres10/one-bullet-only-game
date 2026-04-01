1e5020c8b04c7bc3df46ffcfd731cf098fc575ff
gmandres10 <gman3david@gmail.com>
2026-04-01 04:45:43 +0000

    Apr 1, 2026, 12:45 AM

--- src/main.py
+++ src/main.py
@@ -207,8 +207,6 @@ class Game:
         if self.bullet.active:
             hit = pygame.sprite.spritecollide(self.bullet, self.enemies, True)
             if hit:
-                self.all_sprites.add(self.enemy)
-                self.enemies.add(self.enemy)
                 self.bullet.active = False
                 
             
