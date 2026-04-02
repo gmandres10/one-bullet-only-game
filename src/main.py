3299971f88ca513efee774ee593e1bf89daa0973
gmandres10 <gman3david@gmail.com>
2026-04-02 20:21:24 +0000

    Apr 2, 2026, 4:21 PM

--- src/main.py
+++ src/main.py
@@ -162,7 +162,7 @@ class Game:
         self.enemies = pygame.sprite.Group()
         self.player = Player()
         self.bullet = Bullet()
-        self.enemy = Enemy()
+        self.enemy = Enemy(x=SCREEN_WIDTH, y=FLOOR_Y - 50)
         self.all_sprites.add(self.player, self.enemy, self.bullet)
         self.enemies.add(self.enemy)
         
