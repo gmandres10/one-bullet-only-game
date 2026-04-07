8d11cd1bc878155162be4fe5a9258a45a6944956
gmandres10 <gman3david@gmail.com>
2026-04-07 02:04:46 +0000

    Apr 6, 2026, 10:04 PM

--- src/main.py
+++ src/main.py
@@ -134,10 +134,7 @@ class Bullet(pygame.sprite.Sprite):
         r = BULLET_RADIUS
         self.image = pygame.Surface((r * 2, r * 2), pygame.SRCALPHA)
         pygame.draw.circle(self.image, YELLOW, (r, r), r)
-        self.rect = self.image.get_rect()
-        self.active = False
-        
-        
+        self.rect = self.image.get_rect()        
         self.active = False
         self.x = -200
         self.y = -200
