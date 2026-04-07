80b3f336fe3d28471d6bce3f501cc77adf156cf7
gmandres10 <gman3david@gmail.com>
2026-04-07 01:30:30 +0000

    Apr 6, 2026, 9:30 PM

--- src/main.py
+++ src/main.py
@@ -282,8 +282,8 @@ class Game:
         if self.bullet.active:
             
             if pygame.sprite.collide_rect(self.bullet, self.player):
-                if not self._bullet_in_catch_zone():
-                    self.bullet.active = False
+                self.playing = False
+            
             hit = pygame.sprite.spritecollide(self.bullet, self.enemies, True)
             if hit:
                 self.bullet.active = False
