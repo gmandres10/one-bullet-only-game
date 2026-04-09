23d2425f14933eed93f87cb0e948ee38468fe50d
gmandres10 <gman3david@gmail.com>
2026-04-09 01:56:41 +0000

    Apr 8, 2026, 9:56 PM

--- src/main.py
+++ src/main.py
@@ -369,7 +369,7 @@ class Game:
             b = int(20 + ratio * 100)
             pygame.draw.line(self.screen, (r,g,b), (0, i), (SCREEN_WIDTH, i))
         
-        pygame.draw.line(self.screen, (30, 100, 40), (0, FLOOR_Y), (SCREEN_WIDTH, FLOOR_Y), 3)
+        pygame.draw.line(self.screen, (20, 60, 20), (0, FLOOR_Y), (SCREEN_WIDTH, FLOOR_Y), 4)
         
     def run(self):
         while self.running:
