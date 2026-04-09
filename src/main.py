b7746e47fb64efa767fd7ca3afe045215e8c921d
gmandres10 <gman3david@gmail.com>
2026-04-09 00:45:50 +0000

    Apr 8, 2026, 8:45 PM

--- src/main.py
+++ src/main.py
@@ -236,7 +236,7 @@ class Game:
             
             interval = max(2.0, ENEMY_SPAWN_INTERVAL - self.score * 0.2)
             self.spawn_timer = interval
-             
+
     def _spawn_enemy(self, count):
         for i in range(count):
             side = random.choice(["left", "right"])
